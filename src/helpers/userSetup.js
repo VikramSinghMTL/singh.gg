b1b2973db913912652dfa01ad8ffdc9a1dc3879c
Vikram Singh <vikram.singh@johnabbott.qc.ca>
2024-11-07 21:12:58 -0500

    Added bibliography to bottom of notes.

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -1,10 +1,16 @@
+const mdBiblatex = require('@arothuis/markdown-it-biblatex');
+
 function userMarkdownSetup(md) {
-  // The md parameter stands for the markdown-it instance used throughout the site generator.
-  // Feel free to add any plugin you want here instead of /.eleventy.js
+	md.use(mdBiblatex, {
+		bibPath: './library.bib',
+		bibliographyTitle: '<h2 class="bibliography-title">References</h2>',
+		appendBibliography: true,
+	});
 }
+
 function userEleventySetup(eleventyConfig) {
-  // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
-  // Feel free to add any plugin you want here instead of /.eleventy.js
+	// The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
+	// Feel free to add any plugin you want here instead of /.eleventy.js
 }
 exports.userMarkdownSetup = userMarkdownSetup;
 exports.userEleventySetup = userEleventySetup;
